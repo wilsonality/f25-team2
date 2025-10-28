fde09f7ca449790fd6a862fc5252428f01496850
wilsonality <ptcisbored@gmail.com>
2025-10-27 22:21:03 -0400

    added join column where necessary

--- backend-api/src/main/java/com/team2/spartanslist/mailing_list/MailingList.java
+++ backend-api/src/main/java/com/team2/spartanslist/mailing_list/MailingList.java
@@ -1,10 +1,10 @@
 package com.team2.spartanslist.mailing_list;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,9 +18,9 @@ public class MailingList {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long mailingListID;
 
-    @Column(nullable = false)
+    @JoinColumn(name="sellerID",nullable = false)
     private Long sellerID;
 
-    @Column(nullable = false)
+    @JoinColumn(name="shopperID",nullable = false)
     private Long shopperID;
 }
--- backend-api/src/main/java/com/team2/spartanslist/offer/Offer.java
+++ backend-api/src/main/java/com/team2/spartanslist/offer/Offer.java
@@ -10,6 +10,8 @@ import com.team2.spartanslist.seller.Seller;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
