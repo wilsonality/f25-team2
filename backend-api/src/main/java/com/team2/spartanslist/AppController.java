0b6406b92043b2ea7b61f3f17d88449c45a6d6fb
wilsonality <ptcisbored@gmail.com>
2025-11-12 12:41:48 -0500

    committing

---
+++ backend-api/src/main/java/com/team2/spartanslist/AppController.java
@@ -0,0 +1,12 @@
+package com.team2.spartanslist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping({"", "/"})
+    public String redirectToStudents() {
+        return "redirect:/home";
+    }
+}
